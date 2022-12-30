211083c430aeddd9cf72d1228403984ae244067a
David Arranz <CORREO@ELECTRONICO>
2022-12-30 14:01:30 +0100

    lksdfjg

--- app/src/main/java/org/example/salarymanager/AddActivity.java
+++ app/src/main/java/org/example/salarymanager/AddActivity.java
@@ -1,6 +1,63 @@
 package org.example.salarymanager;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class AddActivity extends AppCompatActivity {
+    EditText etMonto,etFecha,etNombre;
+    ImageView ivIcono;
+    public static final int REQUEST_CODE_IMAGE_SELECT = 1;
+    @SuppressLint("MissingInflatedId")
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+        etMonto = findViewById(R.id.editTextNumberMonto);
+        etFecha = findViewById(R.id.editTextDate);
+        etNombre = findViewById(R.id.editTextNombre);
+        ivIcono = findViewById(R.id.imageViewIconSet);
+
+        ivIcono.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, REQUEST_CODE_IMAGE_SELECT);
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_IMAGE_SELECT && resultCode == RESULT_OK) {
+            Uri imageUri = data.getData();
+            InputStream inputStream = null;
+            try {
+                inputStream = getContentResolver().openInputStream(imageUri);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            Bitmap image = BitmapFactory.decodeStream(inputStream);
+            try {
+                inputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
 }
